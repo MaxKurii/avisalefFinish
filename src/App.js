faf785ccea7cda1632a158ef47f5d95ef16145f7
Max Kurii <kuriimaxym@gmail.com>
2021-02-06 15:30:09 +0200

    deleted route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import { useReducer } from 'react';
 import { BrowserRouter as Router, 
-  Route,
-  Redirect
   } from "react-router-dom";
 import { Header } from './components/header';
 import { Main } from "./components/main";
-import { HomePage } from './pages/HomePage';
 import { initialState, StoreContext} from './store';
 
 import { reducer } from "./store/reducer";
@@ -19,8 +16,8 @@ function App() {
       <Router>
         <Header />
         <Main />
-          <Redirect from="/" to="dashboard" />
-        <Route path="/dashboard" component={HomePage} />
+          
+        
       </Router>
     </StoreContext.Provider>
     
